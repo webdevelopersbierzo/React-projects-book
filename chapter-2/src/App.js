d2de82de6d2b17fe732ce8599762ad51d32a96a8
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-11-06 11:55:04 +0100

    feat: add react-router-dom

--- chapter-2/src/App.js
+++ chapter-2/src/App.js
@@ -1,3 +1,4 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import logo from './assets/logo.svg';
 import Profile from './components/Profile/Profile';
 import './App.css';
@@ -6,8 +7,14 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <Header logo={logo}/>
-      <Profile userName="webdevelopersbierzo" />
+      <BrowserRouter>
+        <Header logo={logo}/>     
+        <Routes>
+          <Route path='/' element={ <Profile userName="webdevelopersbierzo" />} />
+          
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
