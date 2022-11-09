9f963d7803886a94ebb25b0d6927c98649adab07
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-11-09 16:43:25 +0100

    feat: add backlog page

--- chapter-3/project-manager/src/App.js
+++ chapter-3/project-manager/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header.jsx';
+import Backlog from './pages/Backlog/Backlog';
 import Board from './pages/Board/Board.jsx'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Board />
+      <Backlog />
     </div>
   );
 }
